3da59ef8bcd688076a448005267b6b8d25740bee
Sloan Kelly <sloan@codehoose.com>
2022-12-02 12:10:00 -0500

    Day 2 part 2

--- Day2/Day2/Program.cs
+++ Day2/Day2/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Day2
 {
@@ -21,6 +17,43 @@ namespace Day2
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
+        {
+            //Day2Part1();
+            Day2Part2();
+        }
+
+        private static void Day2Part2()
+        {
+            int totalScore = 0;
+
+            using (Stream stream = File.OpenRead("input.txt"))
+            {
+                using (TextReader reader = new StreamReader(stream))
+                {
+                    string line = "";
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        RPS opponent = ToIndex(line[0]);
+                        int playerState = (int)ToIndex(line[2]); // 0 lose, draw, win
+
+                        // Figure out the player's actual strategy based on the state
+                        RPS player = GetPlay(opponent, playerState);
+
+
+                        // shape you selected (1 for Rock, 2 for Paper, and 3 for Scissors)
+                        int playerScore = 1 + (int)player;
+                        playerScore += DetermineWin(opponent, player);
+                        totalScore += playerScore;
+                    }
+
+                }
+            }
+
+            Console.WriteLine($"Total score: {totalScore}");
+            Console.ReadLine();
+        }
+
+        private static void Day2Part1()
         {
             int totalScore = 0;
 
@@ -44,6 +77,18 @@ namespace Day2
 
             Console.WriteLine($"Total score: {totalScore}");
             Console.ReadLine();
+        }
+
+
+        private static RPS GetPlay(RPS opponent, int playerState)
+        {
+            if (playerState == 1)
+                return opponent; // DRAW
+
+            if (playerState == 0)
+                return (RPS)((3 + ((int)opponent) - 1) % 3);
+
+            return (RPS)(((int)opponent + 1) % 3);
         }
 
         private static int DetermineWin(RPS opponent, RPS player)
