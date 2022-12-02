2652e4f189c3f64523f418be4ab489502503331a
Sloan Kelly <sloan@codehoose.com>
2022-12-01 22:08:09 -0500

    Part 2 of Day 1

--- Day1/Day1/Program.cs
+++ Day1/Day1/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Day1
 {
     internal class Program
     {
+        /// <summary>
+        /// https://adventofcode.com/2022/day/1
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            SortedList<int, int> topThree = new SortedList<int, int>();
+
             int elf = 0;
             int totalElves = 0;
             int elfMaxCalories = 0;
@@ -30,7 +37,11 @@ namespace Day1
                                 elfMaxCalories = elfCalories;
                                 elfFound = elf;
                                 Console.WriteLine($"{elfFound:000} {elfCalories}");
+                                
                             }
+
+                            if (!topThree.ContainsKey(elfCalories))
+                                topThree.Add(elfCalories, elfCalories);
 
                             elfCalories = 0;
                             elf++;
@@ -54,6 +65,14 @@ namespace Day1
             }
 
             Console.WriteLine($"{elfMaxCalories} calories held by elf {elfFound + 1} of {totalElves} in {lines} line(s)");
+
+            int totalTopThree = 0;
+            for (int i = topThree.Count-1; i >= topThree.Count - 3; i--)
+            {
+                totalTopThree += topThree.Values[i];
+            }
+
+            Console.WriteLine($"Total top three {totalTopThree}");
             Console.ReadLine();
         }
     }
