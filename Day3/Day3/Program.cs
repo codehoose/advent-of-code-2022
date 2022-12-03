1ac7f30e6f8cf3b56484558faa3086b89f6667b2
Sloan Kelly <sloan@codehoose.com>
2022-12-03 18:42:36 -0500

    Day 3 part 2

--- Day3/Day3/Program.cs
+++ Day3/Day3/Program.cs
@@ -21,28 +21,51 @@ namespace Day3
             {
                 using (TextReader reader = new StreamReader(stream))
                 {
-                    string line = "";
-                    while ((line = reader.ReadLine()) != null)
+                    while (reader.Peek() != -1)
                     {
-                        int containerSize = line.Length / 2; // always exactly half
-
-                        // Brute force version
-                        for (int i = 0; i < containerSize; i++)
+                        ulong[] lines = new ulong[3];
+                        for (int i = 0; i < 3; i++)
                         {
-                            if (line.IndexOf(line[i], containerSize) >= 0)
+                            string line = reader.ReadLine();
+
+                            for (int c = 0; c < line.Length; c++)
                             {
-                                char duplicate = line[i];
-                                int value = duplicate >= 'a' ? duplicate - 'a' + 1 : duplicate - 'A' + 27;
-                                count += value;
-                                break;
-                            }
+                                char ch = line[c];
+                                int value = ch >= 'a' ? ch - 'a' : ch - 'A' + 26;
+                                lines[i] |= (ulong)Math.Pow(2, value);
+                            }                            
                         }
+
+                        // Used bit fields to determine the duplicate chars in each line.
+                        // The AND function above ands the values together until we're left with a
+                        // bit field that indexes to the a-z-A-Z values. Then I convert that to a 
+                        // string and find the first occurence of '1'. Subtract from the length and
+                        // you get the value we need.
+                        ulong output = And(lines[0], lines[1], lines[2]);
+                        string binary = Convert.ToString((long)output, 2);
+                        int weight = binary.Length - binary.IndexOf('1');
+                        count += weight;
                     }
                 }
             }
 
             Console.WriteLine($"Total count: {count}");
             Console.ReadLine();
+        }
+
+        static ulong And(ulong u1, ulong u2, ulong u3)
+        {
+            ulong output = 0;
+            for (int i = 0; i < 64; i++)
+            {
+                ulong bit = (ulong)Math.Pow(2, i);
+                if ((u1 & bit) == bit && (u2 & bit) == bit && (u3 & bit) == bit)
+                {
+                    output |= bit;
+                }
+            }
+
+            return output;
         }
     }
 }
