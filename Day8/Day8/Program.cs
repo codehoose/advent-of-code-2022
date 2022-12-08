5c558b9b98b66b0f4785c5867afaf2b0a68b8e4a
Sloan Kelly <sloan@codehoose.com>
2022-12-08 16:29:03 -0500

    Day 8 part 2

--- Day8/Day8/Program.cs
+++ Day8/Day8/Program.cs
@@ -12,10 +12,37 @@ namespace Day8
         static void Main(string[] args)
         {
             string[] lines = File.ReadAllLines("input.txt");
+            // Part 1
             int count = GetVisible(lines);
-
             Console.WriteLine($"There are {count} trees visible");
+
+            // Part 2
+            int scenicScore = GetScenicScore(lines);
+            Console.WriteLine($"Maximum scenic score is {scenicScore}");
+
             Console.ReadLine();
+        }
+
+        static int GetScenicScore(string[] lines)
+        {
+            int scenicScore = 0;
+            for (int y = 0; y < lines.Length; y++)
+            {
+                for (int x = 0; x < lines[y].Length; x++)
+                {
+                    if (!(y == 0 || x == 0 || y == lines.Length - 1 || x == lines[y].Length - 1))
+                    {
+                        int cellScore = GetCellScore(x, y, lines, -1, 0) *
+                                        GetCellScore(x, y, lines, 1, 0) *
+                                        GetCellScore(x, y, lines, 0, -1) *
+                                        GetCellScore(x, y, lines, 0, 1);
+
+                        scenicScore = cellScore > scenicScore ? cellScore : scenicScore;
+                    }
+                }
+            }
+
+            return scenicScore;
         }
 
         static int GetVisible(string[] lines)
@@ -58,6 +85,26 @@ namespace Day8
             }
 
             return true;
+        }
+
+        static int GetCellScore(int x, int y, string[] lines, int xdir, int ydir)
+        {
+            int sx = x;
+            int sy = y;
+            int treeValue = int.Parse(lines[y][x].ToString());
+            while (x > 0 && y > 0 && y < lines.Length - 1 && x < lines[0].Length - 1)
+            {
+                x += xdir;
+                y += ydir;
+
+                int currentTreeValue = int.Parse(lines[y][x].ToString());
+                if (currentTreeValue >= treeValue)
+                {
+                    break;
+                }
+            }
+
+            return sx != x ? x - sx : y - sy;
         }
     }
 }
