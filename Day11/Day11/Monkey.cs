cf2d17e0710cd673112570d6a8673b64ad21ef22
Sloan Kelly <sloan@codehoose.com>
2022-12-11 18:41:34 -0500

    Day 11 part 1 after I'd started part 2

---
+++ Day11/Day11/Monkey.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day11
+{
+    internal class Monkey
+    {
+        private List<int> _items;
+        private Func<int, int> _operation;
+        private Func<int, int> _test;
+
+        private int _current;
+
+        private int _id;
+
+        private uint _inspected;
+
+        public int CurrentItem => _current;
+
+        public uint Inspected => _inspected;
+
+        public bool HasItems => _items.Count > 0;
+
+        public int Id => _id;
+
+        public void Inspect()
+        {
+            _current = _items[0];
+            _inspected++;
+        }
+
+        public void Worry()
+        {
+            _current = _operation(_current);
+        }
+
+        public void Bored()
+        {
+            _current /= 3;
+        }
+
+        public int ChuckTo()
+        {
+            _items.RemoveAt(0);
+            return _test(_current);
+        }
+
+        public void Receive(int item)
+        {
+            _items.Add(item);
+        }
+
+        public Monkey(int id, IEnumerable<int> items, Func<int, int> operation, Func<int, int> test)
+        {
+            _id = id;
+            _items = new List<int>();
+            _items.AddRange(items);
+            _operation = operation;
+            _test = test;
+        }
+    }
+}
---
+++ Day11/Day11/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Monkey> monkeys = MonkeySerializer.Deserialize("input.txt");
+            for (int i = 0; i < 10000; i++)
+            {
+                foreach (var monkey in monkeys)
+                {
+                    while (monkey.HasItems)
+                    {
+                        monkey.Inspect();
+                        monkey.Worry();
+                        monkey.Bored();
+                        int nextMonkey = monkey.ChuckTo();
+                        monkeys[nextMonkey].Receive(monkey.CurrentItem);
+                    }
+                }
+            }
+
+            var sorted = monkeys.OrderBy(m => m.Inspected)
+                                .ToList();
+            foreach (var m in sorted)
+            {
+                Console.WriteLine($"Monkey {m.Id} inspected items {m.Inspected} times.");
+            }
+
+            uint top = sorted[sorted.Count - 1].Inspected;
+            uint nextTop = sorted[sorted.Count - 2].Inspected;
+
+            Console.WriteLine($"The level of monkey business in this situation can be found by multiplying these together: {top*nextTop}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
